94336c6fe83ccb0f9de1ac8da639498c5ddc3c51
Soham908 <sohampalekar908@gmail.com>
2024-06-16 16:35:09 +0530

    drawer completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,25 +5,19 @@ import DrawerCustom from "./components/DrawerCustom";
 function App() {
 
   return (
-    <Box sx={{ display: "flex", height: '100vh' }}>
+    <Box sx={{ display: "flex", height: '100vh', background: 'black' }}>
       
       <DrawerCustom />
 
-      {/* main area */}
-      {/* <Box
-          component="main"
-          sx={{ flexGrow: 1, p: 3, backgroundColor: "black" }}
-        ></Box> */}
-      <Grid container p={1} direction={"column"} sx={{ flexGrow: 1, height: '100%' }} component="main">
+      <Grid container direction={"column"} sx={{ flexGrow: 1 }} component="main">
         <Grid
-          item
-          xs={7.5}
-          md={7.5}
-          lg={7.5}
-          sx={{ background: "blue", borderRadius: 4 }}
+          item xs={7.5} md={7.5} lg={7.5} sx={{ background: "blue" }}
         >
+
           <Box>Fin goals</Box>
+        
         </Grid>
+        
         <Grid
           item
           xs={4.5}
--- frontend/src/components/DrawerCustom.js
+++ frontend/src/components/DrawerCustom.js
@@ -1,45 +1,67 @@
-import { Drawer, Toolbar, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider } from "@mui/material";
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import { Drawer, Toolbar, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider, Typography, IconButton } from "@mui/material";
+import { useNavigate } from 'react-router-dom'
+import HomeIcon from '@mui/icons-material/Home';
+import LoginIcon from '@mui/icons-material/Login';
+import SavingsIcon from '@mui/icons-material/Savings';
+import ArticleIcon from '@mui/icons-material/Article';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const DrawerCustom = () => {
     const drawerWidth = '15%';
+    const navigate = useNavigate()
   return (
     <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
+    sx={{
+      width: "17%",
+      flexShrink: 0,
+      [`& .MuiDrawer-paper`]: {
+        width: "17%",
+        boxSizing: "border-box",
+        backgroundColor: "#f4f4f4",
+        padding: "0.5%",
+        color: "white",
+        backgroundColor: "#0B0D0E",
+      },
+    }}
         variant="permanent"
         anchor="left"
       >
-        <Toolbar />
+        <Toolbar>
+          <IconButton onClick={() => {navigate("/profile")} }>
+            <AccountCircleIcon sx={{ width: 33, height: 33, color: 'wheat' }}/>
+          </IconButton>
+          <Typography sx={{ marginLeft: 1, fontSize: 22 }}>
+            Soham 
+          </Typography>
+        </Toolbar>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          {
+          [
+            { text: "HomePage", icon: <HomeIcon />, onClick: () => {navigate("/")} },
+            { text: "Finance Goals", icon: <SavingsIcon />, onClick: () => {navigate("/finance-goals")}},
+            { text: "Articles", icon: <ArticleIcon />, onClick: () => {navigate("/articles")}},
+            { text: "Login", icon: <LoginIcon />, onClick: () => {navigate("/login")}},
+          ]
+          .map((item, index) => (
+            <ListItem key={index} disablePadding>
+              <ListItemButton
+              sx={{
+                padding: "8px 16px",
+                "&:hover": { backgroundColor: "black" },
+                "&.Mui-selected": {
+                  backgroundColor: "#d0d0d0",
+                  "&:hover": { backgroundColor: "#c0c0c0" },
+                },
+                borderRadius: 4,
+                marginTop: "5%",
+              }}
+                onClick={item.onClick}
+              >
+                <ListItemIcon sx={{ minWidth: 36, color: "white" }}>
+                  {item.icon}
                 </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary={item.text} />
               </ListItemButton>
             </ListItem>
           ))}
